dc595e0f66a4b297434f2d0db2dcb226c36dd565
benjchand <benchand@gmail.com>
2019-05-14 14:57:11 -0700

    Starting to add components

--- baseball-display/src/App.test.js
+++ baseball-display/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  render(<App />);
 });
